ad29eee472eb6e0bd008b1753d083f2090d6e301
Tommy Thompson <tomthompson828@gmail.com>
2020-11-17 01:30:20 -0500

    trying to fix CORS issue

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(bodyParser.urlencoded({
 
 // Fix CORS issue
 // https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
