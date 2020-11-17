ed93b48f99a587703ca3ca770c1dc827c3d4632c
Tommy Thompson <tomthompson828@gmail.com>
2020-11-17 01:19:56 -0500

    removed nodemon

--- server.js
+++ server.js
@@ -358,5 +358,5 @@ app.get('*', function(req, res) {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening on port:${port}`);
 })
