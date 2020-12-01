b763fad5f9ea3d46a205b69396ab10a0e79d286b
Tommy Thompson <tomthompson828@gmail.com>
2020-11-30 20:17:14 -0500

    deploy to heroku

--- public/javascript/config.js
+++ public/javascript/config.js
@@ -1,4 +1,4 @@
-// var UI_HELPER_API = "http://localhost:3000";
-var UI_HELPER_API = "https://group2-practice-portal.herokuapp.com";
+var UI_HELPER_API = "http://localhost:3000";
+// var UI_HELPER_API = "https://group2-practice-portal.herokuapp.com";
 var INTERNAL_API = window.location.origin;
 
